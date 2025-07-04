ea407841b85981c6ae639e1a6fccf90e8014fcfe
Eric Hydrick <eric.hydrick@gmail.com>
2025-07-04 16:57:26 -0400

    Issue #15 Popping the implementation off the stack

--- internal/server/person/person_handlers.go
+++ internal/server/person/person_handlers.go
@@ -1,23 +1,31 @@
 package person
 
 import (
-	"errors"
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"net/mail"
+	"strings"
+	"text/template"
 
 	"gift-registry/internal/server"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type registration struct {
-	email     string
-	firstName string
-	lastName  string
+	ErrorMessage   string
+	Email          string
+	EmailError     string
+	FirstName      string
+	FirstNameError string
+	LastName       string
+	LastNameError  string
+	successful     bool
 }
 
 const (
@@ -44,7 +52,9 @@ func init() {
 
 }
 
-// Creates a new user account in the person table. The signup is valid if the user has provided a properly formatted email address, a first name, and a last name (we're not tracking any other user details).
+// Creates a new user account in the person table. The signup is valid if the
+// user has provided a properly formatted email address, a first name, and a
+// last name (we're not tracking any other user details).
 func SignupHandler(svr server.ServerUtils) http.Handler {
 
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
@@ -52,76 +62,139 @@ func SignupHandler(svr server.ServerUtils) http.Handler {
 		ctx, span := tracer.Start(req.Context(), "health")
 		defer span.End()
 
-		signup := registration{
-			email:     req.PostFormValue("email"),
-			firstName: req.PostFormValue("firstName"),
-			lastName:  req.PostFormValue("lastName"),
+		userData := registration{
+			Email:      req.PostFormValue("email"),
+			FirstName:  req.PostFormValue("firstName"),
+			LastName:   req.PostFormValue("lastName"),
+			successful: true,
 		}
 
-		validationErrors := signup.validate()
-		if validationErrors != nil {
-			/*
-				TODO:
-				SEND BACK THE FORM WITH THE ERROR MESSAGE(S) FOR FIXING THE VALIDATIONe
-				ISSUES
-			*/
+		userData.validate()
+
+		/*
+			Send the user details back to leave the form populated, but add error
+			messaging (also capture the associated telemetry)
+		*/
+		if userData.EmailError != "" ||
+			userData.FirstNameError != "" ||
+			userData.LastNameError != "" {
+
+			userData.successful = false
+			signupResponse(ctx, span, res, req, svr, userData)
+			return
+
 		}
 
-		/* TODO: CREATE A NEW ACCOUNT IF THE FORM DATA IS VALID */
+		/* Open a transaction so we can rollback on a DB write failure */
+		tx, err := svr.DB.BeginTx(ctx, nil)
+		if err != nil {
+			svr.Logger.ErrorContext(ctx, "Error starting transaction", slog.String("errorMessage", err.Error()))
+			tx.Rollback()
+			userData.successful = false
+		}
 
-		/*
-			TODO:
-			WILL NEED TO CAPTURE THESE FOR THE FAILURE CASE ABOVE
-		*/
-		signupCtr.Add(ctx, 1, metric.WithAttributes(
-			attribute.Bool("successful", true),
-		))
+		_, err = svr.DB.ExecContext(ctx, "INSERT INTO gift_registry.person (firstName, lastName, email) VALUES ($1, $2, $3)", userData.FirstName, userData.LastName, userData.Email)
+		if err != nil {
+			svr.Logger.ErrorContext(ctx, fmt.Sprintf("Error adding person %s %s to the database", userData.FirstName, userData.LastName))
+			userData.successful = false
+			rbErr := tx.Rollback()
+			if rbErr != nil {
+				svr.Logger.ErrorContext(ctx, "Error rolling back the transaction", slog.String("rollbackErrorMsg", rbErr.Error()))
+				userData.ErrorMessage = "Critical database error"
+				signupResponse(ctx, span, res, req, svr, userData)
+				/* The database may be in an invalid state here, panic to force manual intervention */
+				panic(rbErr)
+			}
+			userData.ErrorMessage = "Database error"
+			signupResponse(ctx, span, res, req, svr, userData)
+			return
+		}
 
-		/*
-			I'm not wild about capturing names in the trace, but it gives me a hook for
-			debugging. In a "proper" cloud app, email would be higher cardinality and
-			give a higher chance for anonymity (if a person used an email account that
-			didn't have their name in it). Given that this app is scoped as a
-			self-hosted app for a family, I'm going to assume anyone looking at the
-			logs would recognize the emails anywayS.
-		*/
-		/*
-			TODO: NEED TO FIGURE OUT A WAY TO LOCK OUT THE OBSERVABILITY DATA IN GRAFANA TO MAKE SURE ONLY I SEE IT.
-		*/
-		span.SetAttributes(
-			attribute.Bool("successful", true),
-			attribute.String("firstName", signup.firstName),
-			attribute.String("lastName", signup.lastName),
-		)
-		/* TODO: SHOULD I MOVE THESE MESSAGES TO A MIDDLEWARE WITH A CONTEXT VARAIBLE REFERENCE FOR DATA? */
-		svr.Logger.InfoContext(ctx, fmt.Sprintf("Finished the operation %s", req.URL.Path), slog.Bool("validationErrors", validationErrors != nil), slog.String("validationMessages", validationErrors.Error()))
+		tx.Commit()
+		signupResponse(ctx, span, res, req, svr, userData)
 
 	})
 
 }
 
-func (reg registration) validate() error {
+func signupResponse(ctx context.Context,
+	span trace.Span,
+	res http.ResponseWriter,
+	req *http.Request,
+	svr server.ServerUtils,
+	userData registration) {
 
-	errs := make([]error, 0)
+	templates := svr.Getenv("TEMPLATES_DIR")
+	signupCtr.Add(ctx, 1, metric.WithAttributes(
+		attribute.Bool("successful", userData.successful),
+	))
+	span.SetAttributes(
+		attribute.Bool("successful", false),
+		attribute.String("firstName", userData.FirstName),
+		attribute.String("lastName", userData.LastName),
+	)
+	tmpl, tmplErr := template.ParseFiles(templates+"/index.html", templates+"/signup-form.html", templates+"/login-form.html")
+	if tmplErr != nil {
+		res.WriteHeader(500)
+		res.Write([]byte("Error loading the signup page template!"))
+		return
+	}
 
-	if _, err := mail.ParseAddress(reg.email); err != nil {
+	/*
+		I'm not wild about capturing names in the trace, but it gives me a hook for
+		debugging. In a "proper" cloud app, email would be higher cardinality and
+		give a higher chance for anonymity (if a person used an email account that
+		didn't have their name in it). Given that this app is scoped as a
+		self-hosted app for a family, I'm not going to worry about the potential
+		for overlap when searching on names.
+	*/
+	span.SetAttributes(
+		attribute.Bool("successful", userData.successful),
+		attribute.String("firstName", userData.FirstName),
+		attribute.String("lastName", userData.LastName),
+	)
 
-		errs = append(errs, fmt.Errorf("invalid email address: %s", err.Error()))
+	/* TODO: SHOULD I MOVE THESE MESSAGES TO A MIDDLEWARE WITH A CONTEXT VARAIBLE REFERENCE FOR DATA? */
+	svr.Logger.InfoContext(ctx,
+		fmt.Sprintf("Finished the operation %s", req.URL.Path),
+		slog.String("userData", userData.String()),
+		slog.String("validationErrors", userData.Error()))
+
+	res.WriteHeader(200)
+	tmpl.Execute(res, userData)
+
+}
+
+func (reg *registration) validate() {
+
+	if _, err := mail.ParseAddress(strings.Trim(reg.Email, " ")); err != nil {
+
+		reg.EmailError = "Invalid email address"
 
 	}
 
-	if reg.firstName == "" {
+	if strings.Trim(reg.FirstName, " ") == "" {
 
-		errs = append(errs, fmt.Errorf("first name is a required field"))
-
-	}
-
-	if reg.lastName == "" {
-
-		errs = append(errs, fmt.Errorf("last name is a required field"))
+		reg.FirstNameError = "First name is required"
 
 	}
 
-	return errors.Join(errs...)
+	if strings.Trim(reg.LastName, " ") == "" {
+
+		reg.LastNameError = "Last name is required"
+
+	}
+
+}
+
+func (re registration) String() string {
+
+	return fmt.Sprintf("firstName=%s, lastName=%s", re.FirstName, re.LastName)
+
+}
+
+func (re registration) Error() string {
+
+	return fmt.Sprintf("email=%s, firstName=%s, lastName=%s", re.EmailError, re.FirstNameError, re.LastNameError)
 
 }
