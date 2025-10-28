65576a0d275c8d489bc7e693df07a1df4c44b1b6
Eric Hydrick <eric.hydrick@gmail.com>
2025-10-27 23:32:48 -0400

    feat: Issue #21 Updated the database to support profiles

---
+++ internal/database/migrations/20251027_211700_person_external_id_not_null.sql
@@ -0,0 +1,5 @@
+ALTER TABLE person 
+    ALTER COLUMN external_id SET NOT NULL,
+    ALTER COLUMN first_name SET NOT NULL,
+    ALTER COLUMN last_name SET NOT NULL, 
+    ALTER COLUMN display_name SET NOT NULL;
