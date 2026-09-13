81098e7bee3e5c0fe7dace0c3735cc70c24748f7
Eric Hydrick <eric.hydrick@gmail.com>
2026-09-12 22:48:52 -0400

    Resolved conflicts

--- .jjconflict-base-0/internal/database/migrations/20250803_155000_create_verifications_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS verifications (
-    person_id INTEGER PRIMARY KEY REFERENCES people (person_id), 
-    token VARCHAR(255) NOT NULL, 
-    token_expiration TIMESTAMP, 
-    attempts SMALLINT DEFAULT 0
-);
--- .jjconflict-side-0/insert_session_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO people (person_id, external_id, email, display_name, first_name, last_name, `type`) 
-    VALUES (1, 'succ-disp-name', 'displayName@localhost.com', 'Root', 'Display', 'Named', 'NORMAL'),
